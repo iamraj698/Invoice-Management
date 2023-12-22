041ae7f2b73bad830dce9c22dc866cc80792bc2f
rajesab <rmuddebihal26@gmail.com>
2023-12-22 10:48:10 +0530

    link change

--- src/Components/CheckClient.js
+++ src/Components/CheckClient.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function CheckClient() {
   return (
     <div className=' d-flex justify-content-center align-items-center vh-100  flex-column' >
-      <h3> New client <a href="/ClientProfile">click here</a></h3> 
-      <h3>  Existing Customer  <a href="/invoice">click here</a></h3> 
+      <h3> New client <Link to="/ClientProfile" >click here</Link></h3> 
+      <h3>  Existing Customer <Link to="/invoice">click here</Link></h3> 
     </div>
   )
 }
